730bab15acb1a66c4bfbc4163d2f55b3cfd8569c
Murugesan7 <naanjil.murugesan@gmail.com>
2023-12-27 13:32:52 +0530

    File Added

---
+++ assserts/JS/main.js
@@ -0,0 +1,11 @@
+
+
+console.log(document.body.children[0].textContent)
+
+console.log(document.body.children[1].textContent)
+
+
+console.log(document.body.children[4].children[0].innerText)
+
+
+console.log(document.images.length)
---
+++ assserts/JS/main3.js
@@ -0,0 +1,11 @@
+const inputTxt = document.getElementById("inputTxt");
+
+console.log(inputTxt.value);
+
+const ques = document.getElementById("ques");
+
+console.log(ques.innerText)
+
+const namme = document.querySelector("#inputTxt");
+
+console.log(namme.innerText);
